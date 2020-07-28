6645af77b4d83070c0594c8dee5abb342674f3b6
John McCann <johnmccann@uchicago.edu>
2020-07-28 14:05:09 -0700

    feat(manifest_merging): add job's python files

---
+++ manifest_merging/manifest_merging_job.py
@@ -0,0 +1,75 @@
+"""
+Module for bucket manifest merging using sower-jobs
+"""
+
+import os
+import sys
+import json
+import logging
+import boto3
+
+from gen3.tools.indexing import merge_bucket_manifests
+
+from settings import JOB_REQUIRES
+from utils import (
+    check_user_permission,
+    detect_delimiter,
+    split_s3_path,
+    upload_file_to_s3_and_generate_presigned_url,
+)
+
+LOG_FILE = "/manifest_merging.log"
+INPUT_MANIFESTS_DIRECTORY = "/input_manifests"
+OUTPUT_MANIFEST = "/output_manifest.txt"
+
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+if __name__ == "__main__":
+    input_data_json = json.loads(os.environ["INPUT_DATA"])
+    access_token = os.environ["ACCESS_TOKEN"]
+
+    with open("/creds.json") as creds_file:
+        job_name = "index-object-manifest"
+        creds = json.load(creds_file).get(job_name, {})
+        if not creds:
+            logging.warning(
+                f"No configuration found for '{job_name}' job. "
+                "Attempting to continue anyway..."
+            )
+
+    # Only use provided authz requirement if resources are not empty
+    access_authz_requirement = JOB_REQUIRES
+    if creds.get("job_requires", {}).get("job_access_req"):
+        access_authz_requirement = creds.get("job_requires")
+
+    # check if user has sower and indexing policies
+    is_allowed, message = check_user_permission(access_token, access_authz_requirement)
+    if not is_allowed:
+        logging.error(f"[out]: {message['message']}")
+        sys.exit()
+
+    os.mkdir(INPUT_MANIFESTS_DIRECTORY)
+    s3 = boto3.client("s3")
+    for i, url in enumerate(input_data_json["URLS"]):
+        s3_bucket, s3_object = split_s3_path(url)
+        local_file_path = os.path.join(INPUT_MANIFESTS_DIRECTORY, f"manifest{i}.txt")
+
+        logging.info(f"[out] downloading {url} to {local_file_path}")
+        s3.download_file(s3_bucket, s3_object, local_file_path)
+
+    merge_bucket_manifests(
+        directory=INPUT_MANIFESTS_DIRECTORY,
+        output_manifest_file_delimiter=detect_delimiter(local_file_path),
+        output_manifest=OUTPUT_MANIFEST,
+    )
+
+    output_s3_bucket = creds["bucket"]
+    log_file_presigned_url = upload_file_to_s3_and_generate_presigned_url(
+        output_s3_bucket, LOG_FILE
+    )
+    output_manifest_presigned_url = upload_file_to_s3_and_generate_presigned_url(
+        output_s3_bucket, OUTPUT_MANIFEST
+    )
+
+    logging.info(f"[out] {log_file_presigned_url} {output_manifest_presigned_url}")
---
+++ manifest_merging/utils.py
@@ -0,0 +1,215 @@
+import csv
+import random
+import requests
+from datetime import datetime
+import string
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+
+    letters = string.ascii_lowercase
+    return "".join(random.choice(letters) for i in range(stringLength))
+
+
+def upload_file(
+    file_name,
+    bucket,
+    object_name=None,
+    aws_access_key_id=None,
+    aws_secret_access_key=None,
+):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :aws_access_key_id: string
+    :aws_secret_access_key: string
+    :return: True if file was uploaded, else False
+    """
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    if aws_access_key_id and aws_secret_access_key:
+        s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+        )
+    else:
+        s3_client = boto3.client("s3")
+
+    try:
+        msg = f"upload_file {file_name} in {bucket}, object: {object_name}"
+        logging.info(msg)
+        s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+def create_presigned_url(
+    bucket_name,
+    object_name,
+    aws_access_key_id=None,
+    aws_secret_access_key=None,
+    expiration=3600,
+):
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :aws_access_key_id: string
+    :aws_secret_access_key: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+    # Generate a presigned URL for the S3 object
+    if aws_access_key_id and aws_secret_access_key:
+        s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+        )
+    else:
+        s3_client = boto3.client("s3")
+
+    try:
+        msg = (
+            f"generate_presigned_url {object_name} in {bucket_name}, exp: {expiration}"
+        )
+        logging.info(msg)
+        response = s3_client.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": bucket_name, "Key": object_name},
+            ExpiresIn=expiration,
+        )
+    except ClientError as e:
+        print(e)
+        return None
+    # The response contains the presigned URL
+    return response
+
+
+def upload_file_to_s3_and_generate_presigned_url(
+    bucket_name,
+    filename,
+    aws_access_key_id=None,
+    aws_secret_access_key=None,
+    expiration=3600,
+):
+    """
+    Upload and generate presigned url
+
+    Args:
+        bucket_name(str): the bucket name
+        filename(str): the input file needs to be uploaded
+        aws_access_key_id(str): aws access key id
+        aws_secret_access_key(str): aws secret access key
+        expiration(int): expiration time
+
+    Returns:
+        str: presigned url
+    """
+
+    now = datetime.now()
+
+    if filename and bucket_name:
+        current_time = now.strftime("%m_%d_%y_%H:%M:%S")
+        upload_file_key = "{}_{}.log".format(current_time, randomString(6))
+
+        if upload_file(
+            filename,
+            bucket_name,
+            upload_file_key,
+            aws_access_key_id,
+            aws_secret_access_key,
+        ):
+            presigned_url = create_presigned_url(
+                bucket_name, upload_file_key, aws_access_key_id, aws_secret_access_key
+            )
+            return presigned_url
+
+    return None
+
+
+def check_user_permission(access_token, job_requires):
+    """
+    Check if user has permission to run the job or not
+
+    Args:
+        access_token(str): the access token
+        job_requires(dict): requirements so that job can run
+            {
+                "arborist_url": "http://arborist-service",
+                "job_access_req": (
+                    [
+                        {"resource": "/sower", "action": {"service": "job", "method": "access"}},
+                        {"resource": "/programs", "action": {"service": "indexd", "method": "write"}},
+                    ],
+                )
+            }
+    Returns:
+        bool: if user has permission to run the job or not
+        dict: a message log
+    """
+
+    params = {
+        "user": {"token": access_token},
+        "requests": job_requires["job_access_req"],
+    }
+    response = requests.post(
+        "{}/auth/request".format(job_requires["arborist_url"].strip("/")),
+        headers={"content-type": "application/json"},
+        json=params,
+    )
+    if response.status_code != 200:
+        return (
+            False,
+            {"message": "Can not run the job. Detail {}".format(response.json())},
+        )
+
+    elif not response.json()["auth"]:
+        return (False, {"message": "User does not have privilege to run the job"})
+    else:
+        return True, {"message": "OK"}
+
+
+def split_s3_path(s3_path):
+    """
+    Return bucket and key contained in s3_path
+
+    Args:
+        s3_path(str): s3 path formatted as "s3://{bucket}/{key}"
+
+    Returns:
+        tuple: (bucket, key)
+    """
+    s3_path = s3_path.replace("s3://", "")
+    split_position = s3_path.index("/")
+    bucket = s3_path[:split_position]
+    key = s3_path[split_position + 1 :]
+    return bucket, key
+
+
+def detect_delimiter(dsv_filename):
+    """
+    Detect and return delimiter used in dsv_filename
+
+    Args:
+        dsv_filename(str): path to the delimiter-separated value file whose
+        delimiter is to be detected
+
+    Returns:
+        str: the detected delimiter
+    """
+    with open(dsv_filename) as dsv_file:
+        dialect = csv.Sniffer().sniff(dsv_file.readline())
+    return dialect.delimiter
